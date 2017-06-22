675e9ba451511cb638eb6d0c929851a9cd7175cf
Leon <526247082@qq.com>
2017-06-22 19:18:42 +0800

    Draw move dot.

--- app/src/main/java/com/itheima/circlepageindicator/CirclePageIndicator.java
+++ app/src/main/java/com/itheima/circlepageindicator/CirclePageIndicator.java
@@ -16,16 +16,17 @@ import android.view.View;
 
 public class CirclePageIndicator extends View {
 
-    /**
-     * 点的半径
-     */
+    //点的半径
     private int mDotRadius = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, getResources().getDisplayMetrics());
+    //点与点的间隔
     private int mDotGap = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, getResources().getDisplayMetrics());
 
 
     private ViewPager mViewPager;
     private Paint mDotPaint;
 
+    private int mPosition;
+    private float mPositionOffset;
 
     public CirclePageIndicator(Context context) {
         this(context, null);
@@ -52,14 +53,44 @@ public class CirclePageIndicator extends View {
 
     public void setViewPager(ViewPager viewPager) {
         mViewPager = viewPager;
+        mViewPager.addOnPageChangeListener(mOnPageChangeListener);
     }
+
+    private ViewPager.OnPageChangeListener mOnPageChangeListener = new ViewPager.OnPageChangeListener() {
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            mPosition = position;
+            mPositionOffset = positionOffset;
+            invalidate();
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
 
     @Override
     protected void onDraw(Canvas canvas) {
+        int dotDistance = mDotGap + 2 *  mDotRadius;//点与点之间圆心的距离
+        //循环遍历不动点
         for (int i = 0; i < mViewPager.getAdapter().getCount(); i++) {
-            float cx = mDotRadius + i * (mDotGap + 2 *  mDotRadius);
+            float cx = mDotRadius + i * dotDistance;
             float cy = mDotRadius;
+            mDotPaint.setColor(Color.WHITE);
             canvas.drawCircle(cx, cy, mDotRadius, mDotPaint);
         }
+        //绘制动点
+        mDotPaint.setColor(Color.RED);
+
+        float mMoveCx = mDotRadius + mPosition * dotDistance + dotDistance * mPositionOffset;
+        float mMoveCy = mDotRadius;
+        canvas.drawCircle(mMoveCx, mMoveCy, mDotRadius, mDotPaint);
     }
 }
