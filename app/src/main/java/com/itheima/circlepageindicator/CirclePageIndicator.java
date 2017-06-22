86b7c9643320014c0b04a15bae3d74570d6b6754
Leon <526247082@qq.com>
2017-06-22 18:39:58 +0800

    Setup Projects.

---
+++ app/src/main/java/com/itheima/circlepageindicator/CirclePageIndicator.java
@@ -0,0 +1,26 @@
+package com.itheima.circlepageindicator;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Leon on 2017/6/22.
+ */
+
+public class CirclePageIndicator extends View {
+
+
+    public CirclePageIndicator(Context context) {
+        this(context, null);
+    }
+
+    public CirclePageIndicator(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+    }
+}
---
+++ app/src/main/java/com/itheima/circlepageindicator/MainActivity.java
@@ -0,0 +1,53 @@
+package com.itheima.circlepageindicator;
+
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+
+    private int[] mDataList = {R.mipmap.a, R.mipmap.b, R.mipmap.c, R.mipmap.d};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mViewPager = (ViewPager) findViewById(R.id.vp);
+        mViewPager.setAdapter(mPagerAdapter);
+    }
+
+    private PagerAdapter mPagerAdapter = new PagerAdapter() {
+        @Override
+        public int getCount() {
+            return mDataList.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            ImageView imageView = new ImageView(MainActivity.this);
+            imageView.setImageResource(mDataList[position]);
+            container.addView(imageView);
+            return imageView;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+    };
+}
